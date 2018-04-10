c55cba060d6ea295a3340b36f2a0102d770992cd
NGdev1 <andreichev.m@mail.ru>
2018-04-10 23:09:48 +0300

    admin panel added, site xtml xttp request navigation added

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,47 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    private function siteRenderFullView($parameters)
+    {
+        return $this->render('site/index.html.twig', $parameters);
+    }
+
     /**
      * @Route("/", name="site_index")
      */
-    public function index()
+    public function showMainPageAction(Request $request)
     {
-        return $this->render('site/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        if ($request->isXmlHttpRequest()) {
+            return $this->render(
+                'site/main.html.twig'
+            );
+        } else {
+            return $this->siteRenderFullView([
+                'content' => 'main'
+            ]);
+        }
     }
+
+    /**
+     * @Route("/for-students", name="site_for_students")
+     */
+    public function showForStudentsAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            return $this->render(
+                'site/for_students.html.twig'
+            );
+        } else {
+            return $this->siteRenderFullView([
+                'content' => 'for-students'
+            ]);
+        }
+    }
+
 }
