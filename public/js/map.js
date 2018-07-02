a743542030f124aab9edad912cc52af5feb1f2df
Айдар Низамов <aydar@MacBook-Pro-Ajdar.local>
2018-07-02 22:45:59 +0300

    Map rendering on site added

--- public/js/map.js
+++ public/js/map.js
@@ -3,20 +3,28 @@
  */
 
 ymaps.ready(function () {
-    latLng = [55.90710733249874, 49.30347561836243];
+    latLng1 = [55.788282, 49.132995];
+    latLng2 = [55.789097, 49.120185];
 
     var myMap = new ymaps.Map('map', {
-            center: latLng,
-            zoom: 12
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        });
+        center: [55.788500, 49.126195],
+        zoom: 15
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
 
-    myMap.geoObjects.add(myPlacemark);
+    myPlacemark1 = new ymaps.Placemark(latLng1, {
+        hintContent: 'Автошкола Пилот-М',
+        balloonContent: 'г. Казань, ул, Бутлерова, дом 30'
+    });
+
+    myPlacemark2 = new ymaps.Placemark(latLng2, {
+        hintContent: 'Автошкола Пилот-М',
+        balloonContent: 'г. Казань, ул. Профсоюзная, дом 40-42'
+    });
+
+    myMap.geoObjects.add(myPlacemark1);
+    myMap.geoObjects.add(myPlacemark2);
     myMap.behaviors.disable(['scrollZoom', 'multiTouch']);
 });
 
